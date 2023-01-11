1e63971930e2efbfd3bad4bbd163a9edf4a13cbe
hkan <hermann.kan.dev@gmail.com>
2023-01-11 07:35:01 -0500

    messages test deployv11

--- backend/messages/datasource.ts
+++ backend/messages/datasource.ts
@@ -7,7 +7,7 @@ export const configOptions: DataSourceOptions = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  entities: ['**/*.entity.js'],
+  entities: ['dist/**/*.entity.js'],
 };
 
 export const dbConfig = new DataSource(configOptions);
